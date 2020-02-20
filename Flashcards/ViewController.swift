b329efbfd9cd4f9557d3d0597da771d119becb57
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2020-02-19 19:28:09 -0800

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Taylor Nguyen on 2/19/20.
+//  Copyright © 2020 Taylor Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
